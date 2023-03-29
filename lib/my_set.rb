9c0e2f2a4f795cf9a5381bcb9a534a4575428a45
danielpdaniel <artofjquinn@gmail.com>
2023-03-29 12:24:40 -0400

    commit tree 6ad4b8641451d3e9504a04d2851453b9bc5fcac9

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,62 @@
+require 'pry'
+
 # your code here
+class MySet
+    attr_accessor :hash
+
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def self.[](*nums)
+       self.new(nums)
+    end
+   
+
+    def clear
+        @hash = {}
+        self
+    end
+
+    def each
+        @hash.each do |value|
+            yield value [0]
+        end
+        self
+    end
+
+    def inspect
+        string = ""
+        arr = self.hash.keys
+        arr.each do |key|
+            if string == ""
+                string = "#{key}"
+            else
+                string = string + ", #{key}"
+            end
+        end
+        "#<MySet: {#{string}}>"
+        # pp self.hash
+    end
+end
